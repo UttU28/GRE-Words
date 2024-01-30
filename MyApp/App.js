4e5721917e65fc694d8403efefb17f8d79243af7
UttU28 <utsavmaan28@gmail.com>
2024-01-30 17:16:17 -0600

    Done with Node, now doing FLask, WTFFFFF

--- MyApp/App.js
+++ MyApp/App.js
@@ -15,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
+    alignItems: "center",
   },
 });
 
--- MyApp/components/SwiperCards.tsx
+++ MyApp/components/SwiperCards.tsx
@@ -1,37 +1,84 @@
 import React, { useRef, useState } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
-import VideoPlayer from 'react-native-video-player';
-
-
-const profile = {
-    id: 1,
-    videoURL: 'https://www.playphrase.me/video/5b96b007cc77853d88561aa4/628ea5c3b071e7f3ed0c0162.mp4',
-    name: 'John Doe', // Set the name here
+import { Text, View, StyleSheet, Image, Dimensions, FlatList } from 'react-native';
+const abate = {
+  "meaning": "Reduce, diminish",
+  "wordIndex": 2,
+  "wordUsed": false,
+  "clipsFound": 4,
+  "clipData": {
+    "1": {
+      "videoURL": "https://wallpapers.com/images/featured/deadpool-tzhfez1w8ud2z8aw.jpg",
+      "subtitle": "There lives within the very flame of love... a kind of wick or snuff that will abate it.",
+      "videoInfo": "Hamlet (1996) [03:00:25]"
+    },
+    "2": {
+      "videoURL": "https://www.playphrase.me/video/5b1839dc8079eb4cd4a57f59/629c44b514bb6d3b4fc38840.mp4",
+      "subtitle": "Consumes it with a burning flame that does not abate.",
+      "videoInfo": "Born of Hope (2009) [00:23:20]"
+    },
+    "3": {
+      "videoURL": "https://www.playphrase.me/video/5b96ab52cc77853d885611ef/628b9403b071e717a65fbf21.mp4",
+      "subtitle": "You basically want to try to either aggravate the demon or abate it.",
+      "videoInfo": "Demon House (2018) [00:20:45]"
+    },
+    "4": {
+      "videoURL": "https://www.playphrase.me/video/5b183b3d8079eb4cd4a5a9b8/627826d4b05db515a22da6b1.mp4",
+      "subtitle": "l have unleashed a raging shit storm of epic proportions on the board of trustees of that pissant school that will not abate until those girls seek enrollment elsewhere.",
+      "videoInfo": "Remember Me (2010) [01:33:10]"
+    }
+  },
+  "searched": true
 };
 
-const SwiperCards = () => {
-    const video = useRef(null);
-    const [status, setStatus] = useState({});
+export const cardWidth = Dimensions.get('screen').width * 0.8;
 
-    return (
+const SwiperCards = () => {
+  const video = useRef(null);
+  const [status, setStatus] = useState({});
+  
+  return (
+    <FlatList style={styles.container}
+      data={Object.values(abate.clipData)}
+      keyExtractor={(item) => item.videoURL}
+      renderItem={({ item }) => (
         <View style={styles.card}>
-            <Text style={styles.name}>{profile.name}</Text>
-            {/* <VideoPlayer video={{uri: profile.videoURL}} autoplay={false}></VideoPlayer> */}
+          <Text style={styles.word}>{"abate"}</Text>
+          <Text style={styles.meaning}>{abate.meaning}</Text>
+          <Image style={styles.image} source={{ uri: item.videoURL }} />
+          <Text style={styles.movieName}>{item.videoInfo}</Text>
+          <Text style={styles.subtitleData}>{item.subtitle}</Text>
         </View>
-    );
+      )}
+      horizontal
+      pagingEnabled
+    />
+  );
 };
 
 const styles = StyleSheet.create({
-    name: {
-        textAlign: 'center', // Add this to center the text
-        fontSize: 18, // Set your desired font size
+    container: {
+        width: cardWidth,
+        marginTop: "25%",
+
     },
-    card: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center', // Center the content horizontally
-        backgroundColor: '#ecf0f1',
-    },
+  card: {
+    width: cardWidth,
+    aspectRatio: 1 / 1.67,
+    backgroundColor: '#ecf0f1',
+  },
+  name: {
+    textAlign: 'center', // Add this to center the text
+    fontSize: 18, // Set your desired font size
+  },
+  image: {
+    width: "100%",
+    height: "50%",
+    borderRadius: 15,
+  },
+  word: {textTransform: "uppercase", fontFamily: "Roboto"},
+  meaning: {},
+  movieName: {},
+  subtitleData: {},
 });
 
 export default SwiperCards;
