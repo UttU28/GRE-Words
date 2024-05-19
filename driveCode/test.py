645faba9f91a44f1cfa0341dcaffd8dde818b3cb
UttU28 <utsavmaan28@gmail.com>
2024-05-19 17:37:02 -0500

    sdv

--- driveCode/test.py
+++ driveCode/test.py
@@ -1,37 +1,36 @@
-import requests
+
+import os
+import json
 import subprocess
 
-def download_video(url, output_path):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        with open(output_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=1024):
-                if chunk:
-                    file.write(chunk)
-        print(f"Video downloaded successfully and saved as {output_path}")
-    else:
-        print(f"Failed to download video. Status code: {response.status_code}")
+def concatVideos(videoList, outputPath):
+    with open('videoList.txt', 'w') as file:
+        for video in videoList:
+            file.write(f"file '{video}'\n")
+        # file.write(f"file 'videoResources/endVideo.mp4'\n")
 
-def verify_and_process_video(input_path, output_path):
-    command = [
-        'ffmpeg', '-i', input_path,
-        '-c', 'copy', output_path
+    ffmpeg_command = [
+        'ffmpeg',
+        '-f', 'concat',
+        '-safe', '0',  # Allow unsafe file names
+        '-i', 'videoList.txt',
+        '-c', 'copy',
+        outputPath
     ]
-    try:
-        subprocess.run(command, check=True)
-        print(f"Video processed and saved as {output_path}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error during video processing: {e}")
+    subprocess.run(ffmpeg_command)
+    os.remove('videoList.txt')
 
-# URL of the video to be downloaded
-video_url = "https://s3.us-west-1.wasabisys.com/video-us.playphrase.me/english-storage/5b96add2cc77853d88561744/628b88deb071e717a65fa251.mp4"
-# Path where the video will be saved
-downloaded_video_path = "downloaded_video.mp4"
-# Path for the final processed video
-processed_video_path = "processed_video.mp4"
+with open("videoResources/greWords.json", "r") as allWords:
+    allWordsData = json.load(allWords)
 
-# Download the video
-download_video(video_url, downloaded_video_path)
+for currentWord, wordData in allWordsData.items():
+    videosToMerge = []
+    for i in range(1, wordData['clipsFound'] + 1):
+        allVideoDir = f"mergedVideos/{currentWord}{i}.mp4"
+        if os.path.exists(allVideoDir):
+            videosToMerge.append(allVideoDir)
 
-# Verify and process the video to ensure it has both video and audio
-verify_and_process_video(downloaded_video_path, processed_video_path)
+    if len(videosToMerge) >= 1:
+        print(f"Merging videos for {currentWord.upper()}")
+        outputPath = f'finalVideos/{currentWord.capitalize()}.mp4'
+        concatVideos(videosToMerge, outputPath)
