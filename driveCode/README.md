3377640f5ca157b51ddbd0f48aecb6cc02d024a5
UttU28 <utsavmaan28@gmail.com>
2024-05-19 19:35:44 -0500

    sdv

--- driveCode/README.md
+++ driveCode/README.md
@@ -10,3 +10,4 @@ ffmpeg -i fillerVideo.mp4 -i thisAudio.mp3 -c:v copy -c:a aac -map 0:v:0 -map 1:
 
 ffmpeg -i endVideo.mp4 -i thisAudio.mp3 -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 -shortest oendVideo.mp4
 
+ffmpeg -f concat -i input.txt -vf "fps=30,scale=1920:1080:force_original_aspect_ratio=decrease,pad=1920:1080:(ow-iw)/2:(oh-ih)/2" -b:v 2M -c:a copy output.mp4
--- driveCode/test.py
+++ driveCode/test.py
@@ -1,54 +1,37 @@
-import os
+#!/usr/bin/env python3
+
+import sys
 import subprocess
-import json
 
-def get_video_info(video_file):
-    # Run ffprobe to get video information
-    result = subprocess.run(['ffprobe', '-v', 'error', '-show_format', '-show_streams', '-print_format', 'json', video_file], capture_output=True)
-    
-    # Parse the JSON output
-    info = json.loads(result.stdout)
-    return info
+def merge_videos(output_file, *input_files):
+    # Construct filter_complex string dynamically
+    filter_complex = ''.join(f'[{i}:v][{i}:a]' for i in range(len(input_files)))
+    map_args = ''.join(f'[{i}:a]' for i in range(len(input_files)))
 
-def convert_video(input_file, output_file, video_info):
-    # Extract video and audio codec information
-    video_codec = video_info['streams'][0]['codec_name']
-    audio_codec = video_info['streams'][1]['codec_name']
-    
-    # Extract video bitrate
-    video_bitrate = video_info['streams'][0]['bit_rate']
-    
-    # Use ffmpeg to convert video to same format
-    subprocess.run(['ffmpeg', '-i', input_file, '-c:v', video_codec, '-c:a', audio_codec, '-b:v', str(video_bitrate), output_file])
+    filter_complex += f"concat=n={len(input_files)}:v=1:a=1[outv][outa]"
 
-def merge_videos(video_files, output_file):
-    # Create a list of input files for ffmpeg
-    input_files = []
-    for file in video_files:
-        input_files.extend(['-i', file])
-    
-    # Use ffmpeg to merge videos
-    subprocess.run(['ffmpeg', *input_files, '-filter_complex', 'concat=n={}:v=1:a=1'.format(len(video_files)), '-c:v', 'copy', '-c:a', 'copy', output_file])
 
-# Source video file to extract format information
-source_video = 'mergedVideos/plummet1.mp4'
+    # Run ffmpeg command
+    command = [
+        "ffmpeg",
+        "-hide_banner",
+        "-i",
+        *input_files,
+        "-filter_complex",
+        filter_complex,
+        "-map",
+        "[outv]",
+        "-map",
+        map_args,
+        "-c:v",
+        "libx264",
+        "-c:a",
+        "aac",
+        output_file
+    ]
+    subprocess.run(command)
 
-# Destination folder for converted videos
-output_folder = 'conVieos'
-os.makedirs(output_folder, exist_ok=True)
-
-# Get video information from source video
-source_info = get_video_info(source_video)
-
-# Convert other video files to same format
-video_files_to_convert = ['oendVideo.mp4', 'ofillerVideo.mp4']
-for video_file in video_files_to_convert:
-    output_file = os.path.join(output_folder, os.path.basename(video_file))
-    convert_video(video_file, output_file, source_info)
-
-# List of converted video files
-converted_video_files = [os.path.join(output_folder, file) for file in os.listdir(output_folder)]
-
-# Merge converted videos
-output_video = 'merged_video.mp4'
-merge_videos(converted_video_files, output_video)
+if __name__ == "__main__":
+    output_file = "out.mp4"
+    input_files = ["mergedVideos/plummet1.mp4","ofillerVideo.mp4","mergedVideos/plummet2.mp4","ofillerVideo.mp4","mergedVideos/plummet3.mp4","ofillerVideo.mp4","mergedVideos/plummet4.mp4","oendVideo.mp4"]
+    merge_videos(output_file, *input_files)
